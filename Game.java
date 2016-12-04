e7353ff0a5f87a9679ea645e53d01f20b7889386
Ahmed A Ashraf <ahmed.ashraf@lc.cuny.edu>
2016-12-04 04:18:38 -0500

    Update Game.java

--- Game.java
+++ Game.java
@@ -4,7 +4,6 @@ package asteroids;
 CLASS: Game
 DESCRIPTION: A painted canvas in its own window, updated every tenth second.
 USAGE: Extended by Asteroids.
-NOTE: You don't need to understand the details here, no fiddling neccessary.
 Original code by Dan Leyzberg and Art Simon
 */
 import java.awt.*;
